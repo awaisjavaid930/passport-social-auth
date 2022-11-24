77d46eeb5ff082675ba4573f0aee4011eaa30c24
Awais Javaid <53362912+awaisjavaid930@users.noreply.github.com>
2022-11-24 18:42:14 +0500

    Passport installed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // $this->registerPolicies();
+        // Passport::loadKeysFrom(__DIR__.'/../secrets/oauth');
     }
 }
